29ed8e8abb8bc9c3d62d16ccfea204a4d947db36
Shakya47 <saurabhsh47g@gmail.com>
2023-08-27 15:21:47 +0530

    removed next logo

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -73,9 +73,6 @@ const Sidebar = ({apiLimitCount = 0, isPro=false} : SidebarProps) => {
     <div className=" space-y-4 py-4 flex flex-col h-full bg-[#111827] text-white">
       <div className="px-3 py-2 flex-1">
         <Link href="/" className="flex items-center pl-3 mr-4">
-          <div className=" relative w-8 h-8 mr-4">
-            <Image fill src="/next.svg" alt="logo" />
-          </div>
           <h1 className={cn("text-2xl font-bold", montserrat.className)}>
             Gear-5
           </h1>
